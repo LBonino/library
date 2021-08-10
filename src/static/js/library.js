3cd906033a8cb1a9c3112f33d4150c22f2eafa91
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-08-10 16:02:36 -0300

    Generate and append card for book when submitting form

--- src/static/js/library.js
+++ src/static/js/library.js
@@ -36,14 +36,62 @@ function Book(title, author, pageNumber, isRead) {
     this.isRead = isRead;
 }
 
+function generateCard(book) {
+    const title = document.createElement("h2");
+    const author = document.createElement("p");
+    const pageNumber = document.createElement("p");
+    const readInfo = document.createElement("p");
+    
+    title.classList.add("title");
+    author.classList.add("author");
+    pageNumber.classList.add("page-number");
+    readInfo.classList.add("read-info");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pageNumber.textContent = `${book.pageNumber} pages`;
+    readInfo.textContent = (book.isRead) ? "Already read" : "Not read yet";
+
+    const bookInfo = document.createElement("div");
+    bookInfo.classList.add("book-info");
+    bookInfo.appendChild(title);
+    bookInfo.appendChild(author);
+    bookInfo.appendChild(pageNumber);
+    bookInfo.appendChild(readInfo);
+
+    const markReadButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    markReadButton.classList.add("mark-read");
+    removeButton.classList.add("remove");
+
+    markReadButton.textContent = "Mark as read";
+    removeButton.textContent = "Remove from library";
+
+    const cardButtons = document.createElement("div");
+    cardButtons.classList.add("card-buttons");
+    cardButtons.appendChild(markReadButton);
+    cardButtons.appendChild(removeButton);
+
+    const card = document.createElement("div");
+    card.classList.add("card"); 
+    card.appendChild(bookInfo);
+    card.appendChild(cardButtons);
+
+    return card;
+}
+
+const cardGrid = document.querySelector("#card-grid");
+
 bookForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    library.addBook(new Book(
-            bookForm.elements["form-title"].value,
-            bookForm.elements["form-author"].value,
-            bookForm.elements["form-page-number"].value,
-            bookForm.elements["form-mark-read"].checked
-        )
-    );
+    const book = new Book(
+        bookForm.elements["form-title"].value,
+        bookForm.elements["form-author"].value,
+        bookForm.elements["form-page-number"].value,
+        bookForm.elements["form-mark-read"].checked
+    )
+    library.addBook(book);
+    cardGrid.appendChild(generateCard(book));
     overlay.toggle();
 });
