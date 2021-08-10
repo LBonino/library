249e70a6e278590c3d21edd868e50904c0bab53b
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-08-10 15:12:06 -0300

    Add books to library using form

--- src/static/js/library.js
+++ src/static/js/library.js
@@ -22,3 +22,28 @@ const addBook = document.querySelector("#add");
 addBook.addEventListener("click", overlay.toggle);
 overlay.addEventListener("click", overlay.toggle);
 
+let library = {
+    books: [],
+    addBook: function(book) {
+        this.books.push(book);
+    },
+}
+
+function Book(title, author, pageNumber, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.isRead = isRead;
+}
+
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    library.addBook(new Book(
+            bookForm.elements["form-title"].value,
+            bookForm.elements["form-author"].value,
+            bookForm.elements["form-page-number"].value,
+            bookForm.elements["form-mark-read"].checked
+        )
+    );
+    overlay.toggle();
+});
